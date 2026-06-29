40ef65a9903e8241b6e63f42b383925c95099a02
DamL98 <damian.litewka@gmail.com>
2026-06-29 14:49:28 +0200

    feature/blazor-created-project-starter

---
+++ frontend-blazor/FrontendBlazor/Program.cs
@@ -0,0 +1,31 @@
+using FrontendBlazor.Client.Pages;
+using FrontendBlazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FrontendBlazor.Client._Imports).Assembly);
+
+app.Run();
